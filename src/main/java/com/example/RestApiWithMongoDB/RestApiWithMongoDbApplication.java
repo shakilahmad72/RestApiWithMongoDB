1126dda796afea6a38c450f903dbb8535529e655
shakilahmad72 <iamshakilahmad27@gmail.com>
2026-09-05 09:56:59 +0530

    initial commit

--- src/main/java/com/example/RestApiWithMongoDB/RestApiWithMongoDbApplication.java
+++ src/main/java/com/example/RestApiWithMongoDB/RestApiWithMongoDbApplication.java
@@ -44,28 +44,28 @@ public class RestApiWithMongoDbApplication {
 
             // usingMongoTemplateAndQuery(repository, mongoTemplate, email, student);
 
+            String email = student.getEmail();
+
             repository.findStudentByEmail(email)
                     .ifPresentOrElse(s -> {
 						System.out.println(s + " already exists");
                     }, () -> {
-                        System.out.println("Inserting student " + student);
-                        repository.insert(student);
+						System.out.println("Inserting student " + student);
+						repository.insert(student);
                     });
         };
 
     }
 
-    private void usingMongoTemplateAndQuery(StudentRepository repository, MongoTemplate) {
+    private void usingMongoTemplateAndQuery(StudentRepository repository, MongoTemplate mongoTemplate, String email, Student student) {
 
 		Query query = new Query();
-		query.addCriteria(Criteria.where("email").
-
-				is(email));
+		query.addCriteria(Criteria.where("email").is(email));
 
 		List<Student> students = mongoTemplate.find(query, Student.class);
 
 		if (students.size() > 1) {
-			throw new IllegalStateException("found many students with email " + email);
+			throw new IllegalStateException("Found many students with email " + email);
 		}
 		if (students.isEmpty()) {
 			System.out.println("Inserting student " + student);
