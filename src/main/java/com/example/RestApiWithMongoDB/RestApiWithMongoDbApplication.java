55251db0baf6062a81bcdd4614afb453056a5c10
shakilahmad72 <iamshakilahmad27@gmail.com>
2026-09-05 09:23:13 +0530

    initial commit

--- src/main/java/com/example/RestApiWithMongoDB/RestApiWithMongoDbApplication.java
+++ src/main/java/com/example/RestApiWithMongoDB/RestApiWithMongoDbApplication.java
@@ -15,51 +15,64 @@ import java.util.List;
 @SpringBootApplication
 public class RestApiWithMongoDbApplication {
 
-	static void main(String[] args) {
+    static void main(String[] args) {
 
-		SpringApplication.run(RestApiWithMongoDbApplication.class, args);
+        SpringApplication.run(RestApiWithMongoDbApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner runner(
+            StudentRepository repository, MongoTemplate mongoTemplate) {
+        return args -> {
+            Address address = new Address(
+                    "India",
+                    "Bokaro",
+                    "123456"
+            );
+
+            Student student = new Student(
+                    "Shakil",
+                    "Ahmad",
+                    "example@gmail.com",
+                    Gender.MALE,
+                    address,
+                    List.of("Computer Science", "Maths"),
+                    BigDecimal.TEN,
+                    LocalDateTime.now()
+
+            );
+
+            // usingMongoTemplateAndQuery(repository, mongoTemplate, email, student);
+
+            repository.findStudentByEmail(email)
+                    .ifPresentOrElse(s -> {
+						System.out.println(s + " already exists");
+                    }, () -> {
+                        System.out.println("Inserting student " + student);
+                        repository.insert(student);
+                    });
+        };
+
+    }
+
+    private void usingMongoTemplateAndQuery(StudentRepository repository, MongoTemplate) {
+
+		Query query = new Query();
+		query.addCriteria(Criteria.where("email").
+
+				is(email));
+
+		List<Student> students = mongoTemplate.find(query, Student.class);
+
+		if (students.size() > 1) {
+			throw new IllegalStateException("found many students with email " + email);
+		}
+		if (students.isEmpty()) {
+			System.out.println("Inserting student " + student);
+			repository.insert(student);
+		} else {
+			System.out.println(student + " Already exists");
+		}
+
 	}
-
-	@Bean
-	CommandLineRunner runner(
-			StudentRepository repository, MongoTemplate mongoTemplate) {
-		return args -> {
-			Address address = new Address(
-					"India",
-					"Bokaro",
-					"123456"
-			);
-
-			Student student = new Student(
-					"Shakil",
-					"Ahmad",
-					"example@gmail.com",
-					Gender.MALE,
-					address,
-					List.of("Computer Science", "Maths"),
-					BigDecimal.TEN,
-					LocalDateTime.now()
-
-			);
-
-			Query query = new Query();
-            String email = "email";
-            query.addCriteria(Criteria.where("email").is(email));
-
-			List<Student> students = mongoTemplate.find(query, Student.class);
-
-			if (students.size() > 1) {
-				throw new IllegalStateException("found many students with email " + email);
-			}
-
-			if (students.isEmpty()) {
-				System.out.println("Inserting student " + student);
-				repository.insert(student);
-			} else {
-				System.out.println(student + " Already exists");
-			}
-
-		};
-	}
-
 }
--- src/main/java/com/example/RestApiWithMongoDB/StudentRepository.java
+++ src/main/java/com/example/RestApiWithMongoDB/StudentRepository.java
@@ -2,7 +2,11 @@ package com.example.RestApiWithMongoDB;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface StudentRepository extends MongoRepository<Student, String> {
+import java.util.Optional;
+
+public interface StudentRepository
+        extends MongoRepository<Student, String> {
+    Optional<Student> findStudentByEmail(String email);
 
 
 }
